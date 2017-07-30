8737812b7c7c8f4cc017494c401d416a0f7c692e
Jerom Dreux <jd@izorian.co.uk>
2017-07-30 13:26:29 +0000

    Update Dynamic CSS Injection

--- _includes/scripts/sensitive.js
+++ _includes/scripts/sensitive.js
@@ -1,13 +1,23 @@
 (function(){
-  var createStylesheet = (function() {
+  var createStylesheet = function() {
     var style = document.createElement("style");
     style.setAttribute("type", "text/css");
     style.setAttribute("data-src", "sensitive");
     style.appendChild(document.createTextNode(""));
     document.head.appendChild(style);
-    return style.sheet;
-  });
-  var sheet = createStylesheet();
-  sheet.insertRule(".font-sensitive { visibility: hidden; }");
-  sheet.insertRule(".css-sensitive { display: none; }");
+    return style;
+  };
+  var addRule = function(sheet, selector, rules, index) {
+    if("insertRule" in sheet) {
+      sheet.insertRule(selector + "{" + rules + "}", index);
+    }
+    else if("addRule" in sheet) {
+      sheet.addRule(selector, rules, index);
+    }
+  };
+  
+  var style = createStylesheet();
+  addRule(style.sheet, ".font-sensitive", "visibility: hidden;");
+  addRule(style.sheet, ".css-sensitive", "display: none;");
+  
 })();
